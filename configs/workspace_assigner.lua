ed19394796d3165926f6b8d5bb4fdec1dd87d4d0
Pablo Camara <pablo-c-camara@telecom.pt>
2020-07-13 22:06:20 +0100

    improvements

--- configs/workspace_assigner.lua
+++ configs/workspace_assigner.lua
@@ -4,23 +4,19 @@ local appName = get_application_name();
 debug_print(winName .. " (App name: " .. appName .. ")");
 if appName == "Firefox" or ( winName == "Thunar" or string.match(winName, " - File Manager") ) then
     set_window_workspace(1);
-    change_workspace(1);
-    focus_window();
-elseif winName == "Terminal" then
+   
+elseif winName == "Terminal" or string.match(winName, "Terminal") or string.match(appName, "Terminal") then
     set_window_workspace(5);
-    change_workspace(5);
-    focus_window();
-elseif winName == "Visual Studio Code" then
+   
+elseif winName == "Visual Studio Code" or string.match(winName, "Visual Studio Code") then
         set_window_workspace(4);
-        change_workspace(4);
-        focus_window();
+       
 elseif string.match(winName, "Slack") 
         or string.match(winName, "Microsoft Teams") 
         or winName == "Checking your credentials..." 
         or winName == "Mozilla Thunderbird" then
     set_window_workspace(3);
-    change_workspace(3);
-    focus_window();
+
 else
     if string.match(winName, "- Sublime Text (UNREGISTERED)") 
         or winName == "Postman"
@@ -28,10 +24,14 @@ else
         or winName == "Poedit"
     then
         set_window_workspace(2);
-        change_workspace(2);
-        focus_window();
     end
 end
 
 
 
+if appName == "Devilspie2" or ( winName == "Devilspie2" or string.match(winName,"Devilspie2") ) then
+    set_window_workspace(5);
+    change_workspace(5);
+    focus_window();
+    close_window()
+end
